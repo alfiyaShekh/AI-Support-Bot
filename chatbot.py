0e0fd31417b0fdb7d11d95af5d66fca28e628ef7
Alfiya Shekh <alfiyashekh521@gmail.com>
2026-06-13 17:54:22 +0530

    done

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,44 @@
-print("hello")
+f=open("faqs.txt","r")
+faq=f.read()
+# print(faq)
+
+question=input("Ask a qustion")
+
+# print("\n Questions:")
+# print(question)
+
+# print("\n Faqs:")
+# print(data)
+
+# # f strings
+# name="alfiya"
+# age=20
+# message=f"hello my name is {name} and my age is {age}"
+# #print(message)
+
+# a=17
+# b=8
+# sum=f"sum is {a+b}"
+# # print(sum)
+
+# name=input("Enter ur name")
+# # print(f"hello {name}")
+
+
+################
+prompt=f"""
+You are customer support assistant.
+
+FAQs:
+{faq}
+
+Question:
+{question}
+
+ans:
+"""
+
+print(prompt)
+
+
+
